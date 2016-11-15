fa540b24aa93a7ee0d274f2608b9439c5fafe0f5
Glooory <glooorypu@gmail.com>
2016-11-15 14:35:06 +0800

    update denpendency

--- app/src/main/java/com/glooory/calligraphy/application/MyApplication.java
+++ app/src/main/java/com/glooory/calligraphy/application/MyApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 
 import com.orhanobut.logger.Logger;
 import com.squareup.leakcanary.LeakCanary;
-import com.squareup.picasso.OkHttpDownloader;
-import com.squareup.picasso.Picasso;
 
 /**
  * Created by Glooory on 2016/5/15 0015.
@@ -15,9 +13,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Picasso picasso =  new Picasso.Builder(this).
-                downloader(new OkHttpDownloader(getCacheDir(), 150 * 1024 * 1024)).build();
-        Picasso.setSingletonInstance(picasso);
         Logger.init();
         LeakCanary.install(this);
     }
